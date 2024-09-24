4f395c41bea4311e17180b89769256e50b7b484a
Darren Porter <t1g3rp1nt@gmail.com>
2024-09-24 18:49:06 +0700

    basic packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import icon from 'astro-icon';
+
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    react(),
+    icon(),
+    playformCompress()
+  ]
+});
